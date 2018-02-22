a0e0544f637c485b4082446fe541865dc8588f5f
Qihao He <qi.he@maine.edu>
2018-02-22 11:50:28 -0500

    hello_fftw3 update

--- hello_fftw3.c
+++ hello_fftw3.c
@@ -4,13 +4,12 @@ desciption:
 hello_fftw3.c with function of auto size and output to .csv
 Author:Qihao He
 */
-
 #include <stdlib.h>
-// #include <unistd.h>
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
 #include <fftw3.h>
+// #include <unistd.h>
 
 char Usage[] =
     "Usage: hello_fftw3.bin log2_N [log2_M [loops [RMS_C]]]\n"
@@ -27,7 +26,7 @@ unsigned Microseconds(void) {
 }
 
 int main(int argc, char *argv[]){
-  int i, loops, freq, log2_N, log2_M, N, RMS_C, span_log2_N;
+    int i, loops, freq, log2_N, log2_M, N, RMS_C, span_log2_N;
 
     fftw_complex *in, *out; //in, out buffer
     fftw_plan p; //fftw_plan prepare
@@ -43,11 +42,51 @@ int main(int argc, char *argv[]){
         return -1;
     }
 
-    in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
-    out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
-    p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+    span_log2_N = log2_M - log2_N;
+    double time_elapsed[span_log2_N][loops][4]; //3D array
+    for(i=0; i<span_log2_N; i++){
+        for(j=0; j<loops; j++){
+            for(k=0; k<4; k++){
+                time_elapsed[i][j][k] = 0;
+            }
+        }
+    }
+    double REL_RMS_ERR[span_log2_N][loops]; //2D array
+    for(i=0; i<span_log2_N; i++){
+        for(j=0; j<loops; j++){
+            REL_RMS_ERR[i][j] = 0;
+        }
+    }
 
-    fftw_execute(p); /* repeat as needed */
+    printf("log2_N,N,Init_T,FFT_T,RMS_T,Total_T\n");
+
+    for (k=0; k<loops; k++) {
+        t[0] = Microseconds();
+        in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+        out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+        p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
+
+        t[1] = Microseconds();
+        fftw_execute(p); /* repeat as needed */
+        t[2] = Microseconds();
+
+        if(RMS_C == 1){
+            tsq[0]=tsq[1]=0;
+            for (j=0; j<jobs; j++) {
+                base = fft->out + j*fft->step; // output buffer
+                freq = j+1;
+                for (i=0; i<N; i++) {
+                    double re = cos(2*GPU_FFT_PI*freq*i/N);
+                    tsq[0] += pow(re, 2);
+                    tsq[1] += pow(re - base[i].re, 2) + pow(base[i].im, 2);
+                }
+                REL_RMS_ERR[l][k] = sqrt(tsq[1] / tsq[0]);
+            }
+        }
+        t[3] = Microseconds();
+        printf("%i,%i,%d,%d,%d,%d\n",log2_N + l,N,t[1] - t[0],t[2] - t[1],
+        t[3] - t[2],t[3] - t[0]);
+    }
 
     fftw_destroy_plan(p);
     fftw_free(in);
